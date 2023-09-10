5f3918d5f3b7d026eb283649cfe96ebb0deaa47a
AhmedHamed3699 <Ahmed.Hamed03@eng-st.cu.edu.eg>
2023-09-10 19:51:55 +0300

    Created shell header file

---
+++ shell.h
@@ -0,0 +1,23 @@
+#ifndef SHELL
+#define SHELL
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <errno.h>
+
+#define BUF_SIZE 1024
+#define BUF_FLUSH (-1)
+#define PROMPT "$ "
+
+/************* FUNCTIONS *************/
+
+/* write functions */
+int _putchar(char c);
+int _puts(char *str);
+
+#endif
