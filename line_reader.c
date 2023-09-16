e4747a0f50935e34e922d98439d90dd46e5562ec
Ahmed Hosny <ahmhosny2002@gmail.com>
2023-09-17 02:51:15 +0300

    Fixed memory issues and removed useless dumps - buffer allocates 1 more space for the null terminator (oops) - fixed return type - removed a useless if statement (used in previous tests)

--- line_reader.c
+++ line_reader.c
@@ -8,21 +8,16 @@ ssize_t readline(char **ptr, size_t *n)
 	ssize_t r;
 
 	if (n)
-		buf_size = *n;
+		buf_size = *n < 1 ? 1 : *n;
 	buffer = malloc(buf_size * sizeof(char));
 	do
 	{
 		r = read_char(&c);
 		if (r == -1 || (r < 1 && length == 0))
 			return (free(buffer), -1);
-		if (r == 0 && length != 0)
+		if (length + 1 >= buf_size)
 		{
-			length++;
-			break;
-		}
-		if (length >= buf_size)
-		{
-			new_ptr = _realloc(buffer, buf_size, length + 1);
+			new_ptr = _realloc(buffer, buf_size, length + 2);
 			if (new_ptr == NULL)
 				return (free(buffer), -1);
 			buf_size = length + 1;
@@ -35,8 +30,7 @@ ssize_t readline(char **ptr, size_t *n)
 	if (n)
 		*n = buf_size;
 	*ptr = buffer;
-	printf("String: %s\n", *ptr);
-	return (int) (length);
+	return ((ssize_t) length);
 }
 
 int read_char(char *c)
